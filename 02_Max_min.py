afae2dce11c522477e122e2ad3eb6f32a9be2e22
AlisBrk <free-comp@yandex.ru>
2022-01-20 13:22:24 +0300

    Вывод наибольшего и наименьшего

---
+++ 02_Max_min.py
@@ -0,0 +1,35 @@
+# 02. Даны два числа. Показать большее и меньшее число
+
+def max (arg_1, arg_2):
+    result = arg_1
+    if (arg_1 > arg_2): result = arg_1
+    if (arg_2 > arg_1): result = arg_2
+    return result
+
+def min (arg_1, arg_2):
+    result = arg_1
+    if (arg_1 < arg_2): result = arg_1
+    if (arg_2 < arg_1): result = arg_2
+    return result
+
+print ('Введите первое число', end = ' ')
+first_number = int (input())
+print ('Введите второе число', end = ' ')
+second_number = int (input())
+if (first_number == second_number):
+    print ('Числа равны')
+else:
+    Max = max (first_number,second_number)
+    Min = min (first_number,second_number)
+    print (f'Наибольшее: {Max}, наименьшее: {Min}')
+
+
+# Второй способ:
+# if (first_number > second_number):
+#     print('Большее число: ', first_number)
+#     print('Меньшее число: ', second_number)
+# elif (first_number == second_number):
+#     print('Числа равны')
+# else:
+#     print('Большее число: ', second_number)
+#     print('Меньшее число: ', first_number)
