36c7c356ec60e31521a94789297abf7273e38a24
AlisBrk <free-comp@yandex.ru>
2022-01-20 13:54:13 +0300

    Поиск максимального из трех

---
+++ 04_Max_of_the_three.py
@@ -0,0 +1,18 @@
+# 04. Найти максимальное из трех чисел
+
+def max (numb_1, numb_2, numb_3):
+    result = numb_1
+    if (numb_1 > numb_2 and numb_1 > numb_3): result = numb_1
+    if (numb_2 > numb_1 and numb_2 > numb_3): result = numb_2
+    if (numb_3 > numb_1 and numb_3 > numb_2): result = numb_3
+    return result
+
+print ('Введите три числа: ')
+first_numb = int(input())
+second_numb = int(input())
+third_numb = int(input())
+
+maximum = max (first_numb, second_numb, third_numb)
+print ('Максимальное число:', maximum)
+
+
