50bdf7d5fbbfa5e00348b2298fafb9104629a3cb
AlisBrk <free-comp@yandex.ru>
2022-01-23 01:40:49 +0300

    Квадрат числа - описано решение через метод

--- 00_Square of_the_number.py
+++ 00_Square of_the_number.py
@@ -1,4 +1,12 @@
 #01. Вывести квадрат числа
-print('Введите число', end = ' ')
-numb = int (input())
-print ('Квадрат числа равен', numb**2)
+# print('Введите число', end = ' ')
+# numb = int (input())
+# print ('Квадрат числа равен', numb**2)
+
+
+# через метод:
+def sq(x):
+    return x ** 2
+
+numb = int (input('Введите число: '))
+print ('Квадрат числа равен', sq(numb))
