cc96aad040e401d1424e2d324e4ce2197d8dbac0
AlisBrk <free-comp@yandex.ru>
2022-01-23 13:06:32 +0300

    Поиск наибольшей цифры числа

---
+++ 11_Max_digit_of_number.py
@@ -0,0 +1,21 @@
+# 11 Дано число из отрезка [10, 99]. Показать 
+
+def max_dig(numb):
+    dig_one = numb % 10
+    dig_second = (numb - numb % 10) // 10
+    if ( dig_one > dig_second):
+        return  dig_one
+    elif (dig_second >  dig_one):
+        return dig_second
+    else:
+        return dig_second
+    
+    
+import random
+numb = random.randint(10, 99)
+print ('Дано число из отрезка [10, 99]:', numb)
+print ('Наибольшая цифра числа:', max_dig(numb))
+
+
+
+
