7f8b669e9e04378872ee690e498ec59a19617efc
AlisBrk <free-comp@yandex.ru>
2022-01-23 23:27:20 +0300

    Упрощен подсчет второй цифры числа

--- 11_Max_digit_of_number.py
+++ 11_Max_digit_of_number.py
@@ -1,8 +1,8 @@
-# 11 Дано число из отрезка [10, 99]. Показать 
+# 11 Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
 def max_dig(numb):
     dig_one = numb % 10
-    dig_second = (numb - numb % 10) // 10
+    dig_second = numb // 10
     if ( dig_one > dig_second):
         return  dig_one
     elif (dig_second >  dig_one):
@@ -15,7 +15,3 @@ import random
 numb = random.randint(10, 99)
 print ('Дано число из отрезка [10, 99]:', numb)
 print ('Наибольшая цифра числа:', max_dig(numb))
-
-
-
-
